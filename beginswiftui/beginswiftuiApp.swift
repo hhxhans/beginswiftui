14d373430c444820bf4aa9f764753bb652123ff7
hhxhans <213192693@seu.edu.cn>
2022-12-12 18:32:56 +0800

    Initial Commit

---
+++ beginswiftui/beginswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  beginswiftuiApp.swift
+//  beginswiftui
+//
+//  Created by niudan on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct beginswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
